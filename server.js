1aa75a7a9280ac37c15db86f5e5bff8f1780aa5c
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2025-08-05 23:12:57 +0700

    IP2Location Programming Contest 2025

--- server.js
+++ server.js
@@ -6,31 +6,33 @@ const PORT = process.env.PORT || 3000
 app.use(cors());
 
 app.get("/api/ip", async (req, res) => {
-  try {
-    const response = await fetch("https://ip.ip2location.io/?format=json");
-    const data = await response.json();
-    res.json(data); // ส่งต่อให้ frontend
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch IP data" });
-  }
+    try {
+        const response = await axios.get('https://ip.ip2location.io/?format=json')
+        const data = await response.json()
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch IP' })
+    }
 });
 
 app.get('/api/ip2location', async (req, res) => {
+    const { ip } = req.query
+
     try {
-      const { ip } = req.query
-      const response = await axios.get('https://api.ip2location.io', {
-        params: {
-          key: '46BEA0336C594E84ADDDBFCCA38AB0B0',
-          format: 'json',
-          ip,
-        },
-      })
-      res.json(response.data)
+        const response = await axios.get('https://api.ip2location.io', {
+            params: {
+                key: '46BEA0336C594E84ADDDBFCCA38AB0B0',
+                format: 'json',
+                ip,
+            },
+        })
+        res.status(200).json(response.data)
     } catch (error) {
-      res.status(500).json({ error: error.message })
+        console.error('API error:', error.message)
+        res.status(500).json({ error: 'Failed to fetch IP data.' })
     }
-  })
+})
 
 app.listen(PORT, () => {
-  console.log(`Proxy server running on port ${PORT}`);
+    console.log(`Proxy server running on port ${PORT}`);
 });
