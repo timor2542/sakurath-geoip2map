f3ebd4dc668be69fd1417261b87407b26a498682
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2025-08-06 11:39:40 +0700

    IP2Location Programming Contest 2025

--- api/get-ip.js
+++ api/get-ip.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 export default async function handler(req, res) {
     try {
-      const response = await fetch("https://ip.ip2location.io/");
+      const response = await fetch("https://ip.ip2location.io/?format=txt");
       const textData = await response.text(); // ดึงเป็น text
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.status(200).send(textData);
