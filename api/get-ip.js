bc755d4685893585a42451ba4101722d222a5d5c
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2025-08-06 13:41:20 +0700

    IP2Location Programming Contest 2025

--- api/get-ip.js
+++ api/get-ip.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.get('https://ip.ip2location.io/')
-    const data = await response.text();
+    const response = await fetch('https://ip.ip2location.io/')
+    const text = await response.text()
+    const data = JSON.parse(text)
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(data);
     res.status(200).json(data)
   } catch (error) {
     console.error('Failed to fetch IP:', error)
