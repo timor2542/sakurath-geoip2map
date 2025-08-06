ff65f6556de0dd4fa5b2624c5d9ef4d176e5ebdd
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2025-08-06 11:10:57 +0700

    IP2Location Programming Contest 2025

--- api/get-ip.js
+++ api/get-ip.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     try {
       const response = await fetch('https://ip.ip2location.io/?format=json')
-      const data = await response.json()
+      const data = await response.text()
       res.status(200).json(data)
     } catch (error) {
       console.error('Failed to fetch IP:', error)
