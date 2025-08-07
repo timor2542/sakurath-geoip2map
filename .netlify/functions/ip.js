9c011335cee6e67f5734061626041420ebeb1c4c
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2025-08-07 18:06:34 +0700

    IP2Location Programming Contest 2025

--- .netlify/functions/ip.js
+++ .netlify/functions/ip.js
@@ -1,29 +1,29 @@
-import axios from 'axios'
+const axios = require('axios');
 
-export async function handler(event, context) {
+exports.handler = async function (event, context) {
   try {
-    const API_KEY = process.env.IP2LOCATION_KEY
+    const API_KEY = process.env.IP2LOCATION_KEY;
     if (!API_KEY) {
-      throw new Error('Missing API Key: IP2LOCATION_KEY')
+      throw new Error('Missing API Key: IP2LOCATION_KEY');
     }
 
-    const response = await axios.get(`https://api.ip2location.io/?key=${API_KEY}&format=json`)
-    
+    const response = await axios.get(`https://api.ip2location.io/?key=${API_KEY}&format=json`);
+
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify(response.data)
-    }
+      body: JSON.stringify(response.data),
+    };
   } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify({
         error: 'Function failed',
-        message: err.message
-      })
-    }
+        message: err.message,
+      }),
+    };
   }
-}
+};
